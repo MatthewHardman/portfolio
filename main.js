0b69e68f3169ada55117c7e53c227cbe45269e4f
MatthewHardman <matthew9016@gmail.com>
2023-02-02 15:26:06 -0500

    updated background, added animation

--- main.js
+++ main.js
@@ -17,31 +17,66 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
+renderer.render(scene, camera);
+
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(ambientLight);
 
-//Helpers
+/*Helpers
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(gridHelper);
 const controls = new OrbitControls(camera, renderer.domElement);
+const axesHelper = new THREE.AxesHelper(20);
+scene.add(axesHelper);
+*/
 
 const matthewTexture = new THREE.TextureLoader().load("ProfilePhoto.jpg");
 
 const matthew = new THREE.Mesh(
-  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.BoxGeometry(10, 10, 10),
   new THREE.MeshBasicMaterial({ map: matthewTexture })
 );
 
 scene.add(matthew);
-matthew.position.z = -5;
+matthew.position.z = -20;
 matthew.position.x = 2;
 
-renderer.render(scene, camera);
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+scene.background = spaceTexture;
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  matthew.rotation.y += 0.01;
+  matthew.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
 
 function animate() {
   requestAnimationFrame(animate);
 
-  controls.update();
+  //controls.update();
 
   renderer.render(scene, camera);
 }
